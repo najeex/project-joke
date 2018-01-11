043ec64fb0d0459ef83d9e60c5dceba6c9ec239e
najeex <najeebagra@gmail.com>
2018-01-11 14:06:14 +0530

    Add Spring File

---
+++ src/main/java/najeeb/project/joke/jokeapp/JokeappApplication.java
@@ -0,0 +1,12 @@
+package najeeb.project.joke.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeappApplication.class, args);
+	}
+}
